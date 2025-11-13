db981f9d798823b43928a0a21a70fef417acef2e
Elena German <web-lg@mail.ru>
2025-11-13 19:19:17 +0300

    добавлены проверки параметра функции createObjectFromString()

--- src/createObjectFromString.js
+++ src/createObjectFromString.js
@@ -1,22 +1,34 @@
-const str = 'one.two.three.four.five';
+//const str = {};
+//const str = null;
+//let str;
+//const str = 'one.two.three.four.five';
+const str = '2..3';
+
 
 function createObjectFromString(str) {
-  const arr = str.split('.');
-  let resultObj = {};
   //debugger;
+  if (str && !(typeof str === 'object')) {
+    str = str.toString();
+    let arr = str.split('.');
+    arr = arr.filter(item => item.length >= 1)
+    let resultObj = {};
 
-  arr.reduce((accumulator, item, index, arr) => {
-    if (index === arr.length - 1) {
-      accumulator[item] = '';
-    } else {
-      const nestedObject = {};
-      accumulator[item] = nestedObject;
-      accumulator = nestedObject;
-    }
-    return accumulator;
-  }, resultObj);
+    arr.reduce((accumulator, item, index, arr) => {
+      if (index === arr.length - 1) {
+        accumulator[item] = '';
+      } else {
+        if (item.length) {
+          const nestedObject = {};
+          accumulator[item] = nestedObject;
+          accumulator = nestedObject;
+        }
+      }
+      return accumulator;
+    }, resultObj);
 
-  return resultObj;
+    return resultObj;
+  }
+  else return 'Преобразование не выполнено. Передан некорректный параметр'
 }
 
 /* второй вариант покороче получился
--- src/currying.js
+++ src/currying.js
@@ -23,17 +23,18 @@ function curry(f) {
   let arrArguments = [];
 
   function curryF(a) {
-    arrArguments.push(a);    
+    arrArguments.push(a);
     return curryF;
   }
 
-  function returnValue(){
+  function returnValue() {
     const result = f.apply(this, arrArguments);
-     arrArguments = [];
-     return result;
+    arrArguments = [];
+    return result;
   }
 
-  curryF.toString = () => returnValue(); 
+  curryF.valueOf = () => returnValue();
+  curryF.toString = () => returnValue();
 
   return curryF;
 
@@ -41,10 +42,15 @@ function curry(f) {
 
 
 let curryAddition = curry(addition);
-let currySubtraction = curry(subtraction);
-let curryMultiplication = curry(multiplication);
+//let currySubtraction = curry(subtraction);
+//let curryMultiplication = curry(multiplication);
 
-console.log(+curryAddition(40)(50)(20));   //добавлен +, чтобы вызывался toString у функции
-console.log(+currySubtraction(30)(60)(70));
-console.log(+curryMultiplication(3)(2)(10));
+console.log(+curryAddition(40)(50)(20));
+//console.log(currySubtraction(30)(60)(70));
+//console.log(curryMultiplication(3)(2)(10));
+
+let a = curryAddition(40)(50)(20)
+console.log(a)
+let b = a(20)
+console.log(b)
 
